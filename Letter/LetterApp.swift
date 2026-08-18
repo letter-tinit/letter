fb4ece7124a438de7ee61160a6c6ee29665d2937
letter-tinit <letter.tinit@gmail.com>
2026-08-18 13:36:54 +0700

    Initial Commit

---
+++ Letter/LetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterApp.swift
+//  Letter
+//
+//  Created by Tín Nguyễn on 18/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
